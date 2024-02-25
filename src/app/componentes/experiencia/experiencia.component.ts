23e03fe899dc8a7749df2aa549b42909d1085963
Daniel Moreno <mlmajesticdm@gmail.com>
2024-02-25 13:05:06 -0500

    Avance

--- src/app/componentes/experiencia/experiencia.component.ts
+++ src/app/componentes/experiencia/experiencia.component.ts
@@ -9,5 +9,21 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./experiencia.component.css']
 })
 export class ExperienciaComponent {
+elementoSeleccionado: any;
+claseActiva: number = 1;
 
+   mostrarContenido(opcion: number) {
+  // Ocultar todos los elementos
+  var elementos = document.querySelectorAll('.contenido .opcion');
+  elementos.forEach(function(elemento) {
+    elemento.classList.remove('mostrar');
+    elemento.classList.add('ocultar'); // Agregar clase para animar el margen
+  });
+  
+  this.claseActiva = opcion;
+  // Mostrar solo el elemento seleccionado
+  this.elementoSeleccionado = document.getElementById('opcion' + opcion);
+  this.elementoSeleccionado.classList.add('mostrar');
+  this.elementoSeleccionado.classList.remove('ocultar'); // Quitar clase después de la transición
+}
 }
